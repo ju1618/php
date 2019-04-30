192a27aa1a46c501319a344f74ce11299e283657
ju1618 <juliaefernandez@gmail.com>
2019-04-29 21:53:11 -0300

    actualizando

---
+++ php/clase-09/modificar.php
@@ -0,0 +1,9 @@
+<?php
+  session_start();
+
+  <form method="get" action="procesar.php">
+			<label>Buscar:</label> <br>
+			<input type="text" name="termino_busqueda"> <br>
+			<button type="submit">Incrementar</button>
+</form>
+ ?>
