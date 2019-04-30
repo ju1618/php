d41036b2f0b49642b1585dc487ab597f6aeca5d2
ju1618 <juliaefernandez@gmail.com>
2019-04-29 21:14:43 -0300

    actualizando

---
+++ clase_virtual_26-04/subir.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="master.css">
+  <title>subir</title>
+</head>
+<body>
+  <form method="post" enctype="multipart/form-data">
+    Archivo: <input type="file" name="avatar">
+    <br>
+    <input type="submit" name="" value="Enviar">
+  </form>
+
+</body>
+</html>
