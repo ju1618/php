409fdc24ccdd6aad604084a412084f7e92e15759
ju1618 <juliaefernandez@gmail.com>
2019-05-15 21:27:00 -0300

    nuevos ejercicios

--- clase-10-13-05/prueba.php
+++ clase-10-13-05/prueba.php
@@ -1,14 +1,20 @@
 <?php
-include 'usuario.php';
-$user1= new Usuario("Romi", "3/2", "ro@mi.com");
+require_once 'usuario.php';
+$user1= new Usuario("Romi", "3/2", "ro@mi.com", "123");
 
+echo "<pre>";
 var_dump($user1);
+echo "</pre>";
 
-$user2 = new Usuario("Fede", "25/4", "fe@de.com");
 
+$user2 = new Usuario("Fede", "25/4", "fe@de.com", "345");
+
+echo "<pre>";
 var_dump($user2);
+echo "</pre>";
 
-$user1
+$user1->saludar($user1); /*esto es para poder acceder a las funciones publicas (metodos) de la clase, tengo que acceder via instancia*/
+
 
 
  ?>
--- clase-10-13-05/usuario.php
+++ clase-10-13-05/usuario.php
@@ -5,21 +5,58 @@ class Usuario
   private $nombre;
   private $fechaNacimiento;
   private $email;
+  private $edad;
+  private $contrasenia;
 
 
-  function __construct($userName, $fechaNac, $mail)
+  public function __construct($userName, $fechaNac, $mail, $password)
   {
-    $this->nombre = $userName;
+    $this->setName($userName); /*esto es para que la logica la ponga en el setter y no en el constructor*/
     $this->fechaNacimiento = $fechaNac;
-    $this->email = $mail;
+    $this->setEmail($mail);
+    $this->setPassword($password);
   }
 
-  public function setEmail($mail){
-    $this->email = $mail;
+  public function setEmail(string $mail){
+    if(filter_var($mail, FILTER_VALIDATE_EMAIL)){
+      $this->email = $mail;
+      return;
+    }
+
+    echo "lo que me diste no es un formato valido de email";
+
   }
 
   public function getEmail(){
     return $this->email;
+  }
+
+  public function setName(string $name){
+    $this->nombre = $name;
+  }
+
+  public function getName(){
+    return $this->nombre;
+  }
+
+// edad y no fecha de naciemiento
+  public function setAge(int $age){
+    $this->edad = $age;
+  }
+
+  public function getAge(){
+    return $this->edad;
+  }
+
+  public function setPassword($password){
+
+    $this->contrasenia = password_hash($password, PASSWORD_DEFAULT);
+  }
+
+  public function saludar(Usuario $user){
+
+    echo "Hola $this->nombre";
+
   }
 }
 
